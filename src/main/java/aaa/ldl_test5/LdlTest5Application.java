071a0b7610b2c9488a8e766fd6ee3c71cc44f584
ldl <ldl131710@163.comm>
2019-07-08 15:27:36 +0800

    Initial commit

---
+++ src/main/java/aaa/ldl_test5/LdlTest5Application.java
@@ -0,0 +1,13 @@
+package aaa.ldl_test5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdlTest5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdlTest5Application.class, args);
+    }
+
+}
